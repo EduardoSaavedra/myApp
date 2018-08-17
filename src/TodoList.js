5fc531f5f197251aa1b0458b595bdce4df8b7c2b
EduardoSaavedra <eduardosaavedra1505@gmail.com>
2018-08-17 16:32:56 -0500

    First commit

---
+++ src/TodoList.js
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+
+class TodoList extends Component{
+constructor(props){
+  super(props);
+
+  this.state
+
+  this.addItem = this.addItem.bind(this);
+}
+
+  addItem(e){
+
+}
+  render(){
+    return (
+      <div className = "todoListMain">
+        <div className="header">
+          <form onSubmit={this.addItem}>
+            <input placeholder= "enter task">
+            </input>
+            <button type="submit"> add </button>
+          </form>
+         </div>
+      </div>
+    );
+  }
+}
+
+export default TodoList;
